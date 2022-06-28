e09bb70fefe3af3d58a0b9cf22e0987a26be3f9d
Angela Vegega <angela.vegega@gmail.com>
2022-06-28 23:31:24 +0900

    changed background pic, colors, js

--- public/main.js
+++ public/main.js
@@ -1,12 +1,10 @@
-
-
 function countdown() {
-  let now = new Date()
+  const today = new Date()
   
 
-  let bedtime = new Date(now.getFullYear(), now.getMonth(), now.getDate()).setHours(22)
+  let freedom = new Date(2022, 6, 16)
   
-  let difference = bedtime - now.getTime();
+  let difference = freedom - today.getTime();
   
 
   const hour = Math.floor(difference/1000/60/60)%60;
@@ -16,12 +14,12 @@ function countdown() {
   document.querySelector('p').textContent = `${String(hour).padStart(2, '0')} hours, ${String(minute).padStart(2, '0')} minutes, ${String(second).padStart(2, '0')} seconds`
   setInterval(countdown, 1000);
 
-  if (difference < 0) {
-    document.querySelector('h1').textContent = " COUNTDOWN IS OVER"
-    document.querySelector('p').textContent = "GO TO BED";
-    document.querySelector('body').style.background = 'black';
-    document.querySelector('main').style.color = 'white';
-  }
+  // if (difference < 0) {
+  //   document.querySelector('h1').textContent = " COUNTDOWN IS OVER"
+  //   document.querySelector('p').textContent = "GO TO BED";
+  //   document.querySelector('body').style.background = 'black';
+  //   document.querySelector('main').style.color = 'white';
+  // }
 }
 countdown();
 
