ad11bc80699e57cb45661a513435bbe929cbf016
Angela Vegega <angela.vegega@gmail.com>
2022-06-28 23:07:56 +0900

    set up the server

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const app = express()
+const PORT = 8000
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running at port ${PORT}, you better go catch it`);
+})
